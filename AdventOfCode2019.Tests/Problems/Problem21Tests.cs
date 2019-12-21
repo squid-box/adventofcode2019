648bbcb026e7092d6b04b9478f261e78d002428c
Gitea <gitea@fake.local>
2019-12-21 05:03:04 +0000

    PIG: Adding input and boilerplate for 21

---
+++ AdventOfCode2019.Tests/Problems/Problem21Tests.cs
@@ -0,0 +1,11 @@
+namespace AdventOfCode2019.Tests.Problems
+{
+    using AdventOfCode2019.Problems;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Problem21Tests
+    {
+
+    }
+}
---
+++ AdventOfCode2019/Problems/Problem21.cs
@@ -0,0 +1,14 @@
+namespace AdventOfCode2019.Problems
+{
+    public class Problem21 : Problem
+    {
+        public Problem21() : base(21)
+        {
+        }
+
+        public override string Answer()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
