c5e2a23d82b2bae191e904a3232f0af85723299d
Gitea <gitea@fake.local>
2019-12-24 05:03:04 +0000

    PIG: Adding input and boilerplate for 24

---
+++ AdventOfCode2019/Problems/Problem24.cs
@@ -0,0 +1,14 @@
+namespace AdventOfCode2019.Problems
+{
+    public class Problem24 : Problem
+    {
+        public Problem24() : base(24)
+        {
+        }
+
+        public override string Answer()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
